8cc81689e4f3c9b2148e005ab7c3dead09d1feff
nhnaoto <nh.for.app@gmail.com>
2017-09-08 14:19:48 +0900

    test

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var path = require('path');
 // var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
+
+
 var http = require('http'); //httpモジュール呼び出し
 var server = http.createServer(function (request, response) {
     // リクエストを受けると以下のレスポンスを送信する
     response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
     response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
+    response.write('client_id:' + client_id); 
+    response.write('client_secret:' + client_secret);
     response.end(); // レスポンス送信を完了する
 });
 server.listen(process.env.PORT || 4400);
